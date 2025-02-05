9436ca196ff2a05b654d53ab261c795aa3c96ef7
Shridevi <shridevi1720@gmail.com>
2025-02-05 22:02:13 +0530

    Updated route.py with the reqd changes

--- route.py
+++ route.py
@@ -2,30 +2,36 @@ from fastapi import APIRouter,Depends
 from sqlalchemy.orm import Session
 from operations import Operations
 from db import get_db
+from auth import decodetoken,oauth2bearer
 
 routes=APIRouter()
 
 @routes.post("/tasks/")
-def createtask(taskname:str,status:str="Pending",db:Session=Depends(get_db)):
-    operations = Operations(db)
-    return operations.createtask(taskname, status)
+def createtask(taskname:str,status:str="Pending",db:Session=Depends(get_db), token: str = Depends(oauth2bearer)):
+    user_id = decodetoken(token)
+    operations = Operations(db,user_id)
+    return operations.createtask(taskname,status)
     
 @routes.get("/tasks/{taskid}")
-def gettask(taskid:int,db:Session=Depends(get_db)):
-    operations = Operations(db)
+def gettask(taskid:int,db:Session=Depends(get_db), token: str = Depends(oauth2bearer)):
+    user_id = decodetoken(token)
+    operations = Operations(db,user_id)
     return operations.gettask(taskid)
     
 @routes.get("/tasks/")
-def get_all_tasks(db:Session=Depends(get_db)):
-    operations = Operations(db)
+def get_all_tasks(db:Session=Depends(get_db),token: str = Depends(oauth2bearer)):
+    user_id = decodetoken(token)
+    operations = Operations(db,user_id)
     return operations.get_all_tasks()
 
 @routes.put("/tasks/{taskid}")
-def updatetask(taskid: int,taskname: str=None,status:str=None,db: Session=Depends(get_db)):
-    operations = Operations(db)
+def updatetask(taskid: int,taskname: str=None,status:str=None,db: Session=Depends(get_db),token: str = Depends(oauth2bearer)):
+    user_id = decodetoken(token)
+    operations = Operations(db,user_id)
     return operations.updatetask(taskid,taskname,status)
 
 @routes.delete("/tasks/{taskid}")
-def deletetask(taskid: int,db:Session=Depends(get_db)):
-    operations = Operations(db)
+def deletetask(taskid: int,db:Session=Depends(get_db),token: str = Depends(oauth2bearer)):
+    user_id = decodetoken(token)
+    operations = Operations(db,user_id)
     return operations.deletetask(taskid)
