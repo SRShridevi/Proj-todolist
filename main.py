ce413a357f733b1f4f99f1ec6d95453714478ba1
Shridevi <shridevi1720@gmail.com>
2025-02-05 22:03:34 +0530

    Updated main file with the reqd changes

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from db import engine, Base
 from route import routes  
+from models import Users, Task 
+from auth import router as auth_router 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
+app.include_router(auth_router)
 app.include_router(routes)  
