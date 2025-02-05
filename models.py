12e4bb57ae07cea22ade48c1881d1a59e1a21f5a
Shridevi <shridevi1720@gmail.com>
2025-02-05 21:56:16 +0530

    Added Users class with userid and password inorder to handle auth

--- models.py
+++ models.py
@@ -4,4 +4,11 @@ class Task(Base):
     __tablename__="tasks"
     taskid=Column(Integer, primary_key=True, index=True)  
     taskname=Column(String, index=True)  
-    status=Column(String, default="Pending")  
+    status=Column(String, default="Pending") 
+    user_id = Column(Integer) 
+
+class Users(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)  
+    username = Column(String, unique=True, nullable=False, index=True)  
+    hashedpassword = Column(String, nullable=False)  
